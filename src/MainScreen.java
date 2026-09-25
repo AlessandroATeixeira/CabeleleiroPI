bdbc11bc5600a7208f765f7a04021e475cd266d5
thoma <thomaspietro05@gmail.com>
2026-09-24 22:30:08 -0300

    qualquer coisa

--- src/MainScreen.java
+++ src/MainScreen.java
@@ -60,7 +60,7 @@ public class MainScreen extends JFrame {
         btnClientes = criarBotao("Clientes");
         btnProdutos = criarBotao("Produtos");
         btnCaixa = criarBotao("Caixa");
-        btnRelatorios = criarBotao("Relatórios");
+        btnRelatorios = criarBotao("Coisa");
         btnSair = criarBotaoSair("Sair");
 
         painelBotoes.add(btnClientes);
